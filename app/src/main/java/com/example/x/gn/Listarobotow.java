3a0f186174a6176cc5f1478117390a1334c4081d
Grzegorz Nalberski <64641571+gnalberski@users.noreply.github.com>
2020-05-16 17:52:04 +0200

    [GN] [Robots list] Adding next site activity

--- app/src/main/java/com/example/x/gn/Listarobotow.java
+++ app/src/main/java/com/example/x/gn/Listarobotow.java
@@ -6,19 +6,21 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
-import android.app.Activity;
 import android.view.View.OnClickListener;
+import android.widget.Toast;
+import android.app.Activity;
+import android.widget.Button;
 
-class Listarobotow extends AppCompatActivity {
+public class Listarobotow extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
+//    public void listarobotow1(View view) {
+//
+//        setContentView(R.layout.activity_listarobotow);
+//    }
+
+    protected void onCreate2(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_listarobotow);
-        Intent toy = getIntent();
     }
 
-    public Listarobotow(View view) {
-    }
 }
